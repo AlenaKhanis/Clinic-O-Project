9154ca97e487aaa6e87a7c0c8cbb1cc3b99db5c2
AlenaKhanis <alen4k245@gmail.com>
2024-06-24 14:17:49 +0300

    set data

--- my-data.session.sql
+++
@@ -1,5 +0,0 @@
- SELECT DISTINCT p.*, u.username, u.full_name, u.age, u.email, u.phone 
-                    FROM patients p
-                    INNER JOIN users u ON u.id = p.patient_id              
-                    WHERE p.id = 2;
-
